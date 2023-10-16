a4c2734f2ce2e5f671d984440c66280c542162dd
Donatella <donatella@mbpdedonatella.home>
2023-10-16 18:57:15 +0200

    feat: My first feature

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from blog.models import Collaborator
 
 
 class CollaboratorFilterForm(forms.Form):
@@ -9,3 +10,8 @@ class CollaboratorFilterForm(forms.Form):
     )
 
     magasin = forms.ChoiceField(choices=CHOICES, required=False)
+
+
+class JobFilterForm(forms.Form):
+    job_choices = [("", "Tous")] + list(Collaborator.objects.values_list("job", "job").distinct())
+    job = forms.ChoiceField(choices=job_choices, required=False, label="Filtrer par job")
